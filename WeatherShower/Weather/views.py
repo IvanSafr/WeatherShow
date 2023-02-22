9ca6782f9d7768f51ecb471e94dd86ab2b22085a
ivans <ivansafronov997@gmail.com>
2023-02-22 21:49:31 +0300

    Запрос данных погоды

--- WeatherShower/Weather/views.py
+++ WeatherShower/Weather/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render, redirect
+import requests
 
 # Create your views here.
 
 
 def main(request):
-    return render(request, 'base.html', {'title': 'Home'})
+    appid = "6d55abc0d37e67b465e9ee20f6d69931"  # Мой ключ не работает из-за блокировки в РФ
+    appid = "0fedf62a4b48093d0fe6e0b95fe05ae2"  # Ключ преподавателя OVERONE
+
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid="+ appid
+
+    city = 'Bryansk'
+
+    res = requests.get(url.format(city))
+    print(res.text)
+    return render(request, 'base.html', {'title': 'Главная'})
