8b68c97bb0438d415ea561c586305b2687faf37d
ivans <ivansafronov997@gmail.com>
2023-02-23 17:15:55 +0300

    Запрос данных погоды 2.0

--- WeatherShower/Weather/views.py
+++ WeatherShower/Weather/views.py
@@ -6,12 +6,23 @@ import requests
 
 def main(request):
     appid = "6d55abc0d37e67b465e9ee20f6d69931"  # Мой ключ не работает из-за блокировки в РФ
-    appid = "0fedf62a4b48093d0fe6e0b95fe05ae2"  # Ключ преподавателя OVERONE
+    # appid = "0fedf62a4b48093d0fe6e0b95fe05ae2"  # Ключ преподавателя OVERONE
 
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid="+ appid
 
     city = 'Bryansk'
 
     res = requests.get(url.format(city))
-    print(res.text)
-    return render(request, 'base.html', {'title': 'Главная'})
+    print(res)
+    city_info = {
+        'city': city,
+        'temp': res["main"]['temp'],
+        'icon': res["weather"][0]['icon']
+    }
+
+    context = {
+        "info": city_info
+        }
+
+
+    return render(request, 'page.html', context)
